f40b4ffa7b0da79ff12205d36522167f4e372fdb
Zack <zack@fbts.ca>
2018-10-26 08:54:30 -0600

    Update okapi.js

--- dist/okapi.js
+++ dist/okapi.js
@@ -6,7 +6,10 @@ const glob = require("glob")
 const fs = require("fs")
 const path = require("path")
 
+var createElement = document.createElement
+
 var mdFiles = []
+var currentPage = ""
 
 glob("**/pages/*.md",function(er,files) {
   if (!er) {
@@ -21,3 +24,18 @@ glob("**/pages/*.md",function(er,files) {
 var content = document.getElementById("content"),
   sidebar = document.getElementById("sidenav")
 
+mdFiles.forEach(function(obj) {
+  var elem = createElement("div")
+  elem.id = obj
+  elem.innerHTML = obj
+  elem.onClick = switchPage(elem.innerHTML)
+  sidebar.appendChild(elem)
+}
+
+function bufferFile(relPath) {
+    return fs.readFileSync(path.join(__dirname, relPath)); // zzzz....
+}
+
+var switchPage = function(pageName) {
+  content.innerHTML = toString(bufferFile(pageName))
+}
