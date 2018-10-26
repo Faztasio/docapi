aa19b13b858904715d15f3eaabf5bc5fe1fd15dd
Zack <zack@fbts.ca>
2018-10-26 08:32:15 -0600

    Work on functions

--- dist/okapi.js
+++ dist/okapi.js
@@ -1,1 +1,10 @@
-// WORK IN PROGRESS
+// Distribution Modules
+const markdown = require("./markdown.js")
+
+// Other modules
+const filestream = require("fs")
+const path = require("path")
+
+function bufferFile(relPath) {
+  return fs.readFileSync(path.join(__dirname, relPath)).toString(); // zzzz....
+}
